0c6d93f9d7414fb2cce2f0d979ae06f33560495b
Damaso Sanoja <damaso@workstation.devnet>
2017-11-03 00:25:54 -0400

    plain

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var os = ['Windows','macOS','Linux']
 
-app.get('/os',function(req,res) {
+app.get('/',function(req,res) {
   res.json(os);
 });
 
-app.listen(9000, function () {
+app.listen(9001, function () {
 	console.log('=== app is running at port 9000 ===')
 })
